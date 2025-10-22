b0955a4893bb647647898888fce83977f9ac0b09
kostyarsh <rosh.28@mail.ru>
2025-10-22 19:30:13 +0300

    test cov

--- frontend/cypress/e2e/navigation.cy.ts
+++ frontend/cypress/e2e/navigation.cy.ts
@@ -50,10 +50,3 @@ describe('Навигация по сайту ВИНЕГРЕТ', () => {
     cy.contains('Контакты').should('be.visible')
   })
 })
-
-describe('Intentional fail', () => {
-  it('fails on purpose', () => {
-    cy.visit('http://localhost:5173');
-    cy.contains('___text_which_does_not_exist___').should('be.visible');
-  });
-});
