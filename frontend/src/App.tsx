df7e069785ab3c159d809bbe87319a27d1b81c79
P3325 Смирнова Полина Руслановна <nenenavist@MacBook-Pro-Polina-6.local>
2025-11-11 20:23:56 +0300

    удалила vinegret и переместила всё в src + добавила category-page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Box } from "@mui/material";
 import { store } from './store/store';
 import Header from './components/Header/Header';
 import HeroBanner from './components/HeroBanner/HeroBanner';
+import Footer from "./components/footer/Footer";
+import WhyChooseUsScreen from "./screens/why-choose-us-screen/WhyChooseUsScreen";
+import Assortment from "./screens/assortment/Assortment";
+import CategoryPage from "./screens/category-page/CategoryPage";
 
-const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <div className="min-h-screen bg-white">
-        <Header />
-        <main>
-          <HeroBanner />
-          {/* Здесь будут другие секции */}
-        </main>
-      </div>
-    </Provider>
-  );
-};
+function App() {
+    return (
+        <Provider store={store}>
+            <Router>
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minHeight: '100vh'
+                }}>
+                    <Header />
+                    <Box sx={{ flex: 1 }}> {/* Контент растягивается */}
+                        <Routes>
+                            <Route path="/" element={
+                                <>
+                                    <HeroBanner />
+                                    <Assortment />
+                                    <WhyChooseUsScreen />
+                                </>
+                            } />
+                            <Route path="/category/:categoryId" element={<CategoryPage />} />
+                        </Routes>
+                    </Box>
+                    <Footer />
+                </Box>
+            </Router>
+        </Provider>
+    )
+}
 
 export default App;
