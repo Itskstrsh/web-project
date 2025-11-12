152d825b4ce6f83b4b8a4a75414045521276cb3e
TiWh1te <andersan0711@gmail.com>
2025-11-12 05:38:40 +0300

    40%

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,30 @@
 // App.tsx
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import Header from '../src/components/Header/Header.tsx';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import AppRouter from './router/AppRouter';
 import { store } from './store/store';
+import { theme } from './theme/theme';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <div className="App">
-          <Header />
-          <AppRouter />
-        </div>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <div className="App" style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+            <Header />
+            <main style={{ flex: 1 }}>
+              <AppRouter />
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 };
