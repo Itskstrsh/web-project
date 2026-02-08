9d7dcb5cdba17a644c6af4f87b8ffc318e8bb736
ytenen <ytenene@gmail.com>
2026-02-08 03:03:55 +0300

    text encoder fix

--- frontend/jest.ci.config.cjs
+++ frontend/jest.ci.config.cjs
@@ -12,6 +12,9 @@ module.exports = {
         ['@babel/preset-react', { runtime: 'automatic' }],
         ['@babel/preset-typescript', { allowDeclareFields: true }],
       ],
+      plugins: [
+        'babel-plugin-transform-import-meta'
+      ]
     }],
   },
   moduleFileExtensions: ['ts','tsx','js','jsx','json'],
@@ -19,9 +22,14 @@ module.exports = {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 
   collectCoverage: true,
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'App\.test\.tsx$',
+    'AppRouter\.test\.tsx$',
+    'HeaderNav\.test\.tsx$',
+  ],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}',
     '!**/*.d.ts',
